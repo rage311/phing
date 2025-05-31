93ba95e329ea71ce0793d5268f47852627b12ad7
rage311 <rage311@users.noreply.github.com>
2025-05-30 19:53:42 -0600

    Fully functional now

--- app/Main.hs
+++ app/Main.hs
@@ -12,7 +12,6 @@ import Data.IORef
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BSL
 import Data.List
-import Data.Maybe
 import Data.Time
 import Network.Socket
 import Network.Socket.ByteString (recvFrom, sendAll)
@@ -20,34 +19,53 @@ import Text.Printf
 
 import ICMP
 
-pingListen :: Socket -> Chan Message -> IO ()
-pingListen sock chan = forever $ do
-  (recvData, sockAddr) <- recvFrom sock 1024
-  --print recvData
-  recvTime <- getCurrentTime
-
-  -- skip IP header
-  let icmp = drop 20 $ BS.unpack recvData
-  let ping = decode (BSL.pack icmp) :: PingPacket
-  --print $ "received ping: " <> show ping
-  let refId' = mkRefId (ident ping) (seqNum ping)
-  putStrLn $
-    "Received reply for refId " <> show refId'
-    <> " from: " <> show sockAddr
-  -- mapM_ (printf "%02x ") $ BS.unpack recvData
-  writeChan chan $ PingReceived (refId', recvTime)
-  putStrLn ""
 
 data Message
-  = PingSent (Word32, UTCTime)
-  | PingReceived (Word32, UTCTime)
+  = MsgPingSent (Word32, UTCTime)
+  | MsgPingReceived (Word32, UTCTime, SockAddr)
 
-pingResult :: IO ()
-pingResult = return ()
+type RefId    = Word32
+type SentTime = UTCTime
+type RecvTime = UTCTime
+type Timeout  = Int
 
-mkIdent :: Word16
-mkIdent = 0
+data PingSent = PingSent
+  { sentRefId       :: RefId
+  , time            :: SentTime
+  , timeoutThreadId :: ThreadId
+  }
+  deriving (Eq, Show)
 
+data PingResult
+  = Received
+  | TimedOut
+  deriving (Eq, Show)
+
+
+data PingRef = PingRef
+  { refId      :: Word32 -- ident << 16 | seqNum
+  -- , packet  :: PingPacket
+  , sentTime   :: UTCTime
+  , resultTime :: UTCTime
+  , result     :: PingResult
+  }
+  deriving (Eq, Show)
+
+data PingStats = PingStats
+  { countSent    :: Int
+  , countRecv    :: Int
+  , pctSuccess   :: Double
+  , avgRoundTrip :: Double
+  , minRoundTrip :: Double
+  , maxRoundTrip :: Double
+  }
+  deriving (Eq, Show)
+
+defaultTimeout :: Timeout
+defaultTimeout = 4000 * 1000
+
+
+-- combines id and seqNum to form a unique refId
 mkRefId :: Word16 -> Word16 -> Word32
 mkRefId ident seqNum = fromIntegral ident .<<. 16 .|. fromIntegral seqNum
 
@@ -62,73 +80,101 @@ sendPing sock chan ident seqNum payload = do
 
   sendAll sock $ BS.toStrict $ encode ping
   getCurrentTime >>= \now ->
-    writeChan chan $ PingSent (mkRefId ident seqNum, now)
+    writeChan chan $ MsgPingSent (mkRefId ident seqNum, now)
 
-removeRef :: Word32 -> IORef [(Word32, UTCTime)] -> Bool -> IO ()
-removeRef refId refs timedOut = do
-  rs <- readIORef refs
-  let idx = findIndex (\(refId', _) -> refId' == refId) rs
+pingListen :: Socket -> Chan Message -> IO ()
+pingListen sock chan = forever $ do
+  (recvData, sockAddr) <- recvFrom sock 1024
+  --print recvData
+  recvTime <- getCurrentTime
 
-  when (isJust idx) $ do
-    if timedOut then
-      putStrLn $ "Timed out: " <> show refId
-    else
-      putStrLn $ "DEBUG: Removing ref (not a timeout) " <> show refId
-    modifyIORef' refs $ filter (\(refId', _) -> refId' /= refId)
+  -- skip IP header
+  let icmp = drop 20 $ BS.unpack recvData
+  let ping = decode (BSL.pack icmp) :: PingPacket
+  --print $ "received ping: " <> show ping
+  let refId' = mkRefId (ident ping) (seqNum ping)
+  writeChan chan $ MsgPingReceived (refId', recvTime, sockAddr)
+  putStrLn ""
 
-data PingStatus
-  = Sent
-  -- round trip time
-  | Received Double
-  -- timeout time -- duration or UTCTime for this?
-  | TimedOut Double
-  deriving (Eq, Show)
-
--- data PingResult
---   = PingReply
---   | Timeout
-
-data PingRef = PingRef
-  { refId    :: Word32 -- ident << 16 | seqNum
-  -- , packet    :: PingPacket
-  , sentTime :: UTCTime
-  , recvTime :: Maybe UTCTime
-  , status   :: PingStatus
+initialStats :: PingStats
+initialStats = PingStats
+  { countSent    = 0
+  , countRecv    = 0
+  , pctSuccess   = 100.0
+  , avgRoundTrip = 0.0
+  , minRoundTrip = 0.0
+  , maxRoundTrip = 0.0
   }
-  deriving (Eq, Show)
 
-mkPingRef :: Word32 -> UTCTime -> PingRef
-mkPingRef refId sentTime = PingRef
-  { refId
-  , sentTime
-  , recvTime = Nothing
-  , status   = Sent
-  }
+pingDiffMs :: PingRef -> Double
+pingDiffMs (PingRef { sentTime, resultTime }) = 1000.0 *
+  (realToFrac $ nominalDiffTimeToSeconds (diffUTCTime resultTime sentTime) :: Double)
+
+calcStats :: IORef [PingRef] -> IORef [PingSent] -> IO PingStats
+calcStats refs sent = do
+  refs' <- readIORef refs
+  sent' <- readIORef sent
+  let countSent    = length refs' + length sent'
+  let successRefs  = filter (\x -> result x == Received) refs'
+  let countRecv    = length successRefs
+  let pctSuccess   = 100.0 * (fromIntegral countRecv / fromIntegral countSent)
+  let pingDiffs    = map pingDiffMs successRefs
+  let avgRoundTrip = sum pingDiffs / fromIntegral (length successRefs)
+  let minRoundTrip = minimum pingDiffs
+  let maxRoundTrip = maximum pingDiffs
+  return PingStats
+    { countSent
+    , countRecv
+    , pctSuccess
+    , avgRoundTrip
+    , minRoundTrip
+    , maxRoundTrip
+    }
 
 pingMaster :: Chan Message -> IO ()
 pingMaster chan = do
-  refs    <- newIORef []
+  stats <- newIORef initialStats :: IO (IORef PingStats)
+  refs  <- newIORef [] :: IO (IORef [PingRef])
+  sent  <- newIORef [] :: IO (IORef [PingSent])
+
   forever $ do
     readChan chan >>= \case
-      (PingSent (refId', atTime)) -> do
-        putStrLn $ "Sent: " <> show refId' <> " @ " <> show atTime
-        modifyIORef refs $ \rs -> (refId', atTime) : rs
+      (MsgPingSent (refId', sentTime)) -> do
+        putStrLn $ "Sent: " <> show refId'
+
         -- start timeout timer
-        _ <- forkIO $ do
-          threadDelay $ 4000 * 1000
-          removeRef refId' refs True
+        timeoutThreadId <- forkIO $ do
+          threadDelay defaultTimeout
+          putStrLn $ show refId' <> " timed out"
+          getCurrentTime >>= \now ->
+            modifyIORef refs $ \refs' ->
+              PingRef refId' sentTime now TimedOut : refs'
+        modifyIORef sent $ \sent' ->
+          sent' ++ [PingSent refId' sentTime timeoutThreadId]
         return ()
-      (PingReceived (refId', atTime)) -> do
-        putStrLn $ "Recv: refId " <> show refId' <> " @ " <> show atTime
-        readIORef refs >>= \rs ->
-          case lookup refId' rs of
-            (Just sentAtTime) -> do
-              removeRef refId' refs False
-              putStrLn $ printf "%.1f ms\n"
-                ((realToFrac $
-                    nominalDiffTimeToSeconds (diffUTCTime atTime sentAtTime) :: Double
-                 ) * 1000.0)
-            Nothing -> putStrLn $ "No match for seqNum: " <> show refId'
+
+      (MsgPingReceived (refId', recvTime', sockAddr)) -> do
+        -- putStrLn $ "Recv: refId " <> show refId' <> " @ " <> show recvTime'
+        putStrLn $ "Received reply for refId "
+          <> show refId'
+          <> " from: " <> show sockAddr
+
+        (matches@(match:_ms), noMatch) <- readIORef sent >>= \sent' ->
+          return $ partition (\(PingSent refId _ _) -> refId == refId') sent'
+        if null matches then do
+           putStrLn $ "No match for seqNum: " <> show refId'
+        else do
+          let (PingSent sentId sentTime timeoutThreadId) = match
+          killThread timeoutThreadId
+          writeIORef sent noMatch
+
+          let pingRef = PingRef sentId sentTime recvTime' Received
+          putStrLn $ printf "%.1f ms\n" $ pingDiffMs pingRef
+          modifyIORef refs $ \refs' -> pingRef : refs'
+
+        newStats <- calcStats refs sent
+        print newStats
+        writeIORef stats newStats
 
 main :: IO ()
 main = do
@@ -154,11 +200,16 @@ main = do
 
   let pings =
         map (\seqNum -> do
-          sendPing sock chan ident seqNum "your mom goes to college"
+          let payload = "your mom goes to college" :: BS.ByteString
+          putStrLn $ "Sending " <> show (BS.length payload) <> " bytes to " <> show addr
+          sendPing sock chan ident seqNum payload
           threadDelay intervalSec
         ) [0..]
 
-  _listenThreadId <- forkIO $ pingListen sock chan
+  threadsDone <- newEmptyMVar
+  _listenThreadId <- forkFinally
+    (pingListen sock chan)
+    (\_ -> putMVar threadsDone myThreadId)
   _masterThreadId <- forkIO $ pingMaster chan
 
   forever $ sequence_ pings
